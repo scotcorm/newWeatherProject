58e9463750a55dc2b8984f496e6bfe6106b2cee3
Henman2 <47754370+Henman2@users.noreply.github.com>
2023-11-06 14:05:40 +0000

    refactor codes use fetch function in js

--- script.js
+++ script.js
@@ -1,92 +1,32 @@
-// const btn = document.querySelector('button');
-// const output = document.querySelector('#output');
-// const intake = document.querySelector('#intake'); // Add the id attribute to your input element
-// const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=';
-
-// btn.addEventListener('click', getInput);
-
-// function getInput() {
-//   const xhr = new XMLHttpRequest();
-//   let tempVal = intake.value;
-//   let tempURL = url + tempVal;
-
-//   // Clear previous results
-//   output.innerHTML = '';
-
-//   xhr.onload = function () {
-//     if (xhr.readyState === 4) {
-//       if (xhr.status === 200) {
-//         let data = JSON.parse(xhr.responseText);
-//         outputHTML(data);
-//         // console.log(data);
-//       } else {
-//         // Handle API request errors here
-//         output.innerHTML = 'Please enter a city!';
-//       }
-//     }
-//   };
-
-//   xhr.open('GET', tempURL);
-//   xhr.withCredentials = true;
-//   const ApiHeader = process.env.WEATHER_API_HEADER;
-
-//   const ApiKey = process.env.WEATHER_API_KEY;
-//   xhr.setRequestHeader(ApiHeader, ApiKey);
-//   xhr.setRequestHeader('X-RapidAPI-Host', 'weatherapi-com.p.rapidapi.com');
-
-//   xhr.send();
-// }
-
-// function outputHTML(data) {
-//   // Display the data on the page as needed
-//   // For example, if you want to display the temperature:
-//   output.innerHTML = `<img src="${data.current.condition.icon}"> \n In ${data.location.name} the Temperature is: ${data.current.temp_c}°C`;
-// }
-
-// server.js
-
-// import 'dotenv/config'
-// require('dotenv').config(); // Load environment variables from .env file
-const btn = document.querySelector('button');
-const output = document.querySelector('#output');
-const intake = document.querySelector('#intake');
-const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=';
-import { req_header, req_value } from './server.js';
-
-btn.addEventListener('click', getInput);
-
-function getInput() {
-  const xhr = new XMLHttpRequest();
-  let tempVal = intake.value;
-  let tempURL = url + tempVal;
-
-  // Clear previous results
-  output.innerHTML = '';
-
-  xhr.onload = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        let data = JSON.parse(xhr.responseText);
-        outputHTML(data);
-        // console.log(data);
-      } else {
-        // Handle API request errors here
-        output.innerHTML = 'Please enter a city!';
-      }
+const btn = document.getElementById('submit');
+const output = document.getElementById('output');
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': process.env.WEATHER_API_KEY,
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+btn.addEventListener('click', getWeather);
+async function getWeather(){
+  const cityName = document.getElementById('city').value;
+  const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${cityName}`;
+  // console.log(cityName);
+  // console.log(url);
+  try {
+    if(!cityName){
+      output.innerHTML = 'Please enter a city!';
+      throw new Error('Please enter a city');
     }
-  };
-
-  xhr.open('GET', tempURL);
-  xhr.withCredentials = true;
-
-  xhr.setRequestHeader(req_header, req_value); // Use the API key from environment variables
-  xhr.setRequestHeader('X-RapidAPI-Host', 'weatherapi-com.p.rapidapi.com');
-
-  xhr.send();
+    const response = await fetch(url, options);
+    const result = await response.json();
+    displayWeather(result);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function outputHTML(data) {
-  // Display the data on the page as needed
-  // For example, if you want to display the temperature:
-  output.innerHTML = `<img src="${data.current.condition.icon}"> \n In ${data.location.name} the Temperature is: ${data.current.temp_c}°C`;
+function displayWeather(weatherData) {
+  output.innerHTML = `<img src="${weatherData.current.condition.icon}" alt="weather icon"> \n In ${weatherData.location.name} currently the Temperature is: ${weatherData.current.temp_c}°C`;
 }
